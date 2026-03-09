c979ae24e0c3a4c32957b46995f28a7086f4517e
Saurav Pandey <pandeysaurav108@gmail.com>
2026-03-09 22:39:31 +0530

    .

--- SubsequenceCheck.java
+++ SubsequenceCheck.java
@@ -2,6 +2,7 @@ public class SubsequenceCheck {
     public static boolean isSubsequence(String s, String t) {
         int i = 0, j = 0;
 
+        
         while (i < s.length() && j < t.length()) {
             if (s.charAt(i) == t.charAt(j)) {
                 i++;
